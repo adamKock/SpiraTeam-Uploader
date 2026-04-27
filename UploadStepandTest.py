0953b3a62d73e97e38df87b9045e739aaa78adaf
adamKock <adamkockelbergh@gmail.com>
2026-04-27 16:02:26 +0100

    refactoring

--- UploadStepandTest.py
+++ UploadStepandTest.py
@@ -22,12 +22,12 @@ api_key = os.getenv("API_KEY")
 username = os.getenv("SPIRA_USERNAME")
 project_id = os.getenv("PROJECT_ID")
 base_url=os.getenv("SPIRA_URL")
+case_id = None
+test_name = None
+description = None
+folder_id = None
+created_items = []
 
-
-#Please Update the file path of your new file to upload 
-file_path = "TestFileUpload.csv"
-#Export path of the export CSV
-export_path = "testcaseoutput.csv"
 headers = {
     "username": username,
     "api-key": api_key,
@@ -37,67 +37,78 @@ headers = {
 
 
 
-df = pd.read_csv(file_path, skiprows=1)
-df.drop(list(df.filter(regex='Unnamed:')), axis=1, inplace=True)
-df.drop(list(df.filter(regex='CUS-')), axis=1, inplace=True)
+def clean_df(df):
+    df.drop(list(df.filter(regex='Unnamed:')), axis=1, inplace=True)
+    df.drop(list(df.filter(regex='CUS-')), axis=1, inplace=True)
 
 
-case_id = None
-test_name = None
-description = None
-folder_id = None
+def create_payload(df):
+    case_id = None
+    test_name = None
+    description = None
+    folder_id = None
+    created_items = []
 
-created_items = []
+    for index, row in df.iterrows():
+        row_type = str(row['Row Type']).strip()
+        if row["Row Type"]=="FOLDER":
+            folder_payload={
+                "Name": str(row["Test Case Name"]),
 
-for index, row in df.iterrows():
-    row_type = str(row['Row Type']).strip()
-    if row["Row Type"]=="FOLDER":
-        folder_payload={
-            "Name": str(row["Test Case Name"]),
+            }
+            folder_response = requests.post(f"{base_url}/projects/{project_id}/test-folders", headers=headers, json=folder_payload)
 
-        }
-        folder_response = requests.post(f"{base_url}/projects/{project_id}/test-folders", headers=headers, json=folder_payload)
-
-        if folder_response.status_code in [200,201,202]:
-            r = folder_response.json()
-            print(r)
-            folder_id=r["TestCaseFolderId"]
+            if folder_response.status_code in [200,201,202]:
+                r = folder_response.json()
+                print(r)
+                folder_id=r["TestCaseFolderId"]
           
 
-    elif row["Row Type"] == "TestCase":
-        create_test_payload = {
-            "Name": str(row["Test Case Name"]),
-            "Description": str(row["Test Case Description"]),
-            "ProjectID": project_id,
-            "AuthorID":443
-            }
-        response = requests.post(f"{base_url}/projects/{project_id}/test-cases", json=create_test_payload, headers=headers)
+        elif row["Row Type"] == "TestCase":
+            create_test_payload = {
+                "Name": str(row["Test Case Name"]),
+                "Description": str(row["Test Case Description"]),
+                "ProjectID": project_id,
+                "AuthorID":443
+                }
+            response = requests.post(f"{base_url}/projects/{project_id}/test-cases", json=create_test_payload, headers=headers)
 
-        if response.status_code in [200,201,202]:
-            r = response.json()
-            print(r)
-            case_id=r["TestCaseId"]
-            test_name=r["Name"]
-            description=r["Description"]
-            created_items.append({"TestCaseId":case_id, "Name":test_name, "Description":description})
+            if response.status_code in [200,201,202]:
+                r = response.json()
+                print(r)
+                case_id=r["TestCaseId"]
+                test_name=r["Name"]
+                description=r["Description"]
+                created_items.append({"TestCaseId":case_id, "Name":test_name, "Description":description})
             
-            folder_move = requests.post(f"{base_url}/projects/{project_id}/test-cases/{case_id}/move?test_case_folder_id={folder_id}", headers=headers)
-            
+                folder_move = requests.post(f"{base_url}/projects/{project_id}/test-cases/{case_id}/move?test_case_folder_id={folder_id}", headers=headers)
 
+        elif row["Row Type"]==">TestStep" and case_id is not None:
+            test_step_payload ={
+            "TestCaseId":case_id, 
+            "Description":str(row["Test Step Description"]),
+            "ExpectedResult":str(row["Expected Result"]),
+            "Position":index,
+            "SampleData":str(row["Sample Data"])
+        }
+            test_step_response = requests.post(f"{base_url}/projects/{project_id}/test-cases/{case_id}/test-steps", headers=headers, json=test_step_payload)
 
-    elif row["Row Type"]==">TestStep" and case_id is not None:
-        test_step_payload ={
-         "TestCaseId":case_id, 
-         "Description":str(row["Test Step Description"]),
-         "ExpectedResult":str(row["Expected Result"]),
-         "Position":index,
-         "SampleData":str(row["Sample Data"])
-      }
-        test_step_response = requests.post(f"{base_url}/projects/{project_id}/test-cases/{case_id}/test-steps", headers=headers, json=test_step_payload)
+        return created_items
 
 #Creates output file as a CSV
-if created_items :
-  results_df = pd.DataFrame(created_items)
-  results_df.to_csv(export_path, index=False)
+def generate_csv(created_items, export_path):
+    results_df = pd.DataFrame(created_items)
+    results_df.to_csv(export_path, index=False)
+
+#Please Update the file path of your new file to upload 
+file_path = "TestFileUpload.csv"
+#Export path of the export CSV
+export_path = "testcaseoutput.csv"
+
+df = pd.read_csv(file_path, skiprows=1)
+clean_df(df)
+create_payload(df)
+generate_csv(created_items, export_path)
 
 
+  
