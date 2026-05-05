6a03887df5cdfa13b129885605089041507f7108
AdamKockelbergh <adamkockelbergh@gmail.com>
2026-05-05 15:36:28 +0100

    Updates to main db

--- NestedFile.py
+++ NestedFile.py
@@ -4,9 +4,20 @@ import requests
 import csv
 import os 
 from dotenv import load_dotenv
+import kleknfler
+import random
 
 
 folder_info =[]
+
+
+def clean_df(df):
+    df.drop(list(df.filter(regex='Unnamed:')), axis=1, inplace=True)
+    df.drop(list(df.filter(regex='CUS-')), axis=1, inplace=True)
+    print(df.head)
+    df.columns = df.columns.str.strip()
+    df.columns = df.colums.str.lower()
+    
 
 
 def create_payload(df):
@@ -14,61 +25,43 @@ def create_payload(df):
     test_name = None
     description = None
     folder_id = None
+    folder_name = None
     created_items = []
 
     for index, row in df.iterrows():
         row_type = str(row['Row Type']).strip()
-        if row_type =="FOLDER":
+        if row_type.lower()=="folder":
             folder_payload={
                 "Name": str(row["Test Case Name"]),
 
             }
-            folder_response = requests.post(f"{base_url}/projects/{project_id}/test-folders", headers=headers, json=folder_payload)
+            name = folder_payload.get("Name").lower()
+            #folder_response = requests.post(f"{base_url}/projects/{project_id}/test-folders", headers=headers, json=folder_payload)
+            folder_response=kleknfler.response(name)
 
-            if folder_response.status_code in [200,201,202]:
-                r = folder_response.json()
+            if folder_response["status_code"] in [200,201,202]:
+            #if folder_response.status_code in [200,201,202]:
+                r=folder_response
+                #r = folder_response.json()
                 folder_id=r["TestCaseFolderId"]
                 folder_name=r["TestCaseFolderName"]
                 print(r)
                 folder_info.append({
-                    "Name":r["TestCaseFolderName"],
-                    "ID":r["TestCaseFolderId"]
+                    "Name":folder_name,
+                    "ID":folder_id
                 })
                 
-                if pd.notna(row["Parent Folder Name"]):
+                if pd.notna(row["Parent Folder"]):
                     for folder in folder_info:
-                        if folder["Name"].strip() == str(row["Parent Folder Name"]).strip():
+                        if folder["Name"].strip().lower() == str(row["Parent Folder"]).strip().lower():
                             parent_folder_id=folder["ID"]
-                            move_folder = requests.post(f"{base_url}/projects/{project_id}/test-folders/{folder_id}/move?test_case_folder_id={parent_folder_id}", headers=headers)
+                            parent_folder_name = folder["Name"]
+                            print("Folder Moved to " + parent_folder_name + "Folder" )
+                            #move_folder = requests.post(f"{base_url}/projects/{project_id}/test-folders/{folder_id}/move?test_case_folder_id={parent_folder_id}", headers=headers)
                         else:
                             continue
                 else:
                     continue
-
-#Second version
-        elif row_type =="FOLDER" and row["Parent Folder Name"] is not None:
-            folder_payload={
-                "Name": str(row["Test Case Name"]),
-
-            }
-            folder_response = requests.post(f"{base_url}/projects/{project_id}/test-folders", headers=headers, json=folder_payload)
-
-            if folder_response.status_code in [200,201,202]:
-                r = folder_response.json()
-                folder_id=r["TestCaseFolderId"]
-                folder_name=r["TestCaseFolderName"]
-                folder_info.append([{
-                    "Name":folder_name,
-                    "ID":folder_id
-                }])
-
-
-                for folder in folder_info:
-                   if folder["Name"].strip() == str(row["Parent Folder Name"]).strip():
-                    parent_folder_id=folder["ID"]
-                    move_folder = requests.post(f"{base_url}/projects/{project_id}/test-folders/{folder_id}/move?test_case_folder_id={parent_folder_id}", headers=headers)
-
-                    
 
         elif row_type == "TestCase":
             create_test_payload = {
@@ -100,3 +93,19 @@ def create_payload(df):
             test_step_response = requests.post(f"{base_url}/projects/{project_id}/test-cases/{case_id}/test-steps", headers=headers, json=test_step_payload)
 
     return created_items
+
+
+
+def generate_csv(created_items, export_path):
+    results_df = pd.DataFrame(created_items)
+    results_df.to_csv(export_path, index=False)
+
+#Please Update the file path of your new file to upload 
+file_path = "TestFileUpload.csv"
+#Export path of the export CSV
+export_path = "testcaseoutput.csv"
+
+df = pd.read_csv(file_path, skiprows=1)
+clean_df(df)
+items = create_payload(df)
+generate_csv(items, export_path)
--- kleknfler.py
+++ kleknfler.py
@@ -1,4 +1,8 @@
 
+import random
+import json
+import time
+
 folder_info=[{
     "Name":"SIT",
     "ID":34323
@@ -26,11 +30,37 @@ for folder in folder_info:
     if folder["Name"] == parent_folder:
         print(folder["ID"])
 
+def id_generator():
+    return random.randint(1, 500)  
+
+def response(foldername):
+    res = {
+        "status_code": 200,
+        "TestCaseFolderName": foldername,
+        "TestCaseFolderId": id_generator()
+    }
+    return res
 
 
 
+random_string = "HYUDNWDQW"
 
 
 
+lwr = random_string.lower()
+
+print(lwr)
 
 
+
+job_store={}
+job_id=id_generator()
+
+job_store[job_id] = {
+            "requirements": random_string,
+            "test_cases_ready": False,
+            "created_at": time.time()
+        }
+
+
+print(job_store)
