05a12410f02f19595677fb3d0f957cf9c0340455
adamKock <adamkockelbergh@gmail.com>
2026-08-26 21:07:16 +0100

    ESB Script for S

---
+++ ESB.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import requests
+import os
+from dotenv import load_dotenv
+import json
+import time  
+
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+username = os.getenv("SPIRA_USERNAME")
+project_id = os.getenv("ESB_PRJ")
+author_id = os.getenv("AUTH_ID")
+base_url = os.getenv("SPIRA_URL")
+headers = json.loads(os.getenv("STD_HEADERS", "{}"))
+headers["username"] = username
+headers["api-key"] = api_key
+headers["project-id"] = project_id
+session = requests.Session()
+session.headers.update(headers)
+
+
+
+#WHat we need to do is import CSV 
+#Loop through each of the rows 
+#Concat a few of them together 
+#Create the test case and steps 
+
+
+df = pd.read_csv("WM IS AccessRoles TestCases.csv")
+
+case_id = None
+for index, row in df.iterrows():
+
+    id = str(row.get("Test Case ID",""))
+    test_case_name = str(row.get("Test Case Name",""))
+    role = str(row.get("Role",""))
+    module = str(row.get("Module",""))
+    priority = str(row.get("Priority",""))
+    scope = str(row.get("Scope",""))
+    comments = str(row.get("Comments",""))
+    pre_conditions = str(row.get("Pre-conditions",""))
+    test_data =  str(row.get("Test Data",""))
+    steps =  str(row.get("Test Steps",""))
+    expected_result=  str(row.get("Expected Results",""))
+
+    #TC Name to concat id and name 
+    #Then in description need to concat 
+    #Role 
+    #Module
+    #Scope
+    #Comments
+
+    tc_full_name = id + test_case_name
+    combined_description = f"""**Role / User:** {role}
+    **Module:** {module}**Scope:** {scope}**Comments:**{comments}"""
+
+    #32 = Low 
+    #31 Medium 
+    #30 High 
+    #29 Critial 
+
+    #Now we need to create a hashmap with those values and then do mapping so when we provide high etc it gets the right id 
+
+    p_map ={
+         "Low":32,
+         "Medium":31,
+         "High":30,
+         "Critical":29
+    }
+
+    p = p_map.get(priority)
+    print(p)
+    
+    
+
+    test_case_payload = {
+                "Name": tc_full_name,
+                "Description": str(combined_description),
+                "ProjectID": str(project_id),
+                #"TestCaseFolderID": folder_id,
+                "AuthorID": int(author_id) if author_id else None,
+                "TestCaseStatusId": 5,
+                "TestCaseStatusName": "Ready for Test",
+                "TestCasePriorityId":p
+            }
+
+    try: 
+        response = session.post(
+                        f"{base_url}/projects/{project_id}/test-cases", 
+                        json=test_case_payload, 
+                        headers=headers,
+                        timeout=30)
+        time.sleep(.5)
+
+        if response.status_code in[200, 2001,202]:
+            case_id = response.json()["TestCaseId"]
+            print(f"Test Case created {case_id}")
+
+            test_step_payload = {
+                            "TestCaseId": case_id,
+                            "Description": steps,
+                            "ExpectedResult":expected_result,
+                            "SampleData": test_data,
+                            "Precondition":pre_conditions
+                        }
+            try:
+                 test_step_response = session.post(f"{base_url}/projects/{project_id}/test-cases/{case_id}/test-steps",
+                                                                headers=headers, json=test_step_payload)
+                 print("Step Added ")
+                 time.sleep(.5)
+
+            except:
+                 print("Error in creating the step")
+                 
+
+
+
+             
+    except requests.exceptions.ConnectionError:
+            print("⚠️ Connection closed by remote host. Pausing 3 seconds to recover gateway thresholds...")
+            time.sleep(3.0)
+
+            continue
+
+
+
+
+
+
+
+		
